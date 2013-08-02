c453719501f4b0bce11bd4e9d83797c15f249d35
Jason.w <jason.sixsteps@qq.com>
2013-08-02 18:02:43 +0800

    add one

--- src/com/taxmodel/main/MainWindow.java
+++ src/com/taxmodel/main/MainWindow.java
@@ -7,13 +7,19 @@
  */
 package com.taxmodel.main;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+
+import org.rickysun.swingsheet.CellPanel;
 
 /**
  * 
@@ -24,7 +30,15 @@ public class MainWindow extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private CellPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu newMenu;
+	private JMenu dataMenu;
+	private JMenu chartMenu;
+	private JMenuItem tMenuItem;
+	private JMenuItem loadMenuItem;
+	private JMenuItem exportMenuItem;
+	private JMenuItem makeChartMenuItem;
 
 	/**
 	 * Launch the application.
@@ -51,9 +65,44 @@ public class MainWindow extends JFrame {
 		Dimension screenSize =
 				java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 		setBounds(0, 0, screenSize.width - 5, screenSize.height - 80);
-		contentPane = new JPanel();
+		// ------------------------------------------------------------------------------
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		// ------------------------------------------------------------------------------
+		newMenu = new JMenu("New");
+		menuBar.add(newMenu);
+		// ------------------------------------------------------------------------------
+		tMenuItem = new JMenuItem("T101");
+		newMenu.add(tMenuItem);
+		// ------------------------------------------------------------------------------
+		dataMenu = new JMenu("Data");
+		menuBar.add(dataMenu);
+		// ------------------------------------------------------------------------------
+		loadMenuItem = new JMenuItem("Load");
+		dataMenu.add(loadMenuItem);
+		// ------------------------------------------------------------------------------
+		exportMenuItem = new JMenuItem("Export");
+		dataMenu.add(exportMenuItem);
+		// ------------------------------------------------------------------------------
+		chartMenu = new JMenu("Chart");
+		menuBar.add(chartMenu);
+		// ------------------------------------------------------------------------------
+		makeChartMenuItem = new JMenuItem("Make Chart");
+		chartMenu.add(makeChartMenuItem);
+		// ------------------------------------------------------------------------------
+
+		contentPane = new CellPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		try {
+			UIManager
+					.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}
+		catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		SwingUtilities.updateComponentTreeUI(this);
+		// contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 	}
 
